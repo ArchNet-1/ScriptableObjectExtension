0700c8631024f50c87cb8d3f0031b7d599bc80cf
Archaons <louispakel@gmail.com>
2021-03-29 22:20:01 +0200

    First Commit

---
+++ Editor/Scripts/ScriptableObjectExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using UnityEditor;
+
+namespace ArchNet.Extension.ScriptableObject
+{
+    /// <summary>
+    /// [EXTENSION] - [ARCH NET] - [SCRIPTABLE OBJECT] Scriptable object extension manager
+    /// author : LOUIS PAKEL
+    /// </summary>
+    public static class ScriptableObjectExtension
+    {
+        /// <summary>
+        /// Description : Get the object name of a instance
+        /// </summary>
+        /// <param name="pInstanceId"></param>
+        /// <returns></returns>
+        public static string GetScriptablePathName(int pInstanceId)
+        {
+            string assetPath = AssetDatabase.GetAssetPath(pInstanceId);
+
+            string lResult = Path.GetFileNameWithoutExtension(assetPath);
+
+            return lResult;
+        }
+    }
+}
